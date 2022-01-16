2b163b6fe2fda3ba55f4b7f607b20ca96fdd11c4
Marianny Fidelis <mariannyfidelis@gmail.com>
2022-01-16 15:01:41 -0300

    Atualizando os arquivos

--- connection_database.py
+++ connection_database.py
@@ -1,10 +1,17 @@
 import MySQLdb
+import configparser
 
 class FabricaConexao():
-        
+
     @staticmethod
     def conectar():
         MySQLdb.paramstyle = "format"
-        db = MySQLdb.connect(user="root", passwd="mysql", db="clientes", host="localhost", port=3306)
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        db = MySQLdb.connect(user=config['DATABASE']['user'],
+                             passwd=config['DATABASE']['passwd'],
+                             db=config['DATABASE']['db'],
+                             host=config['DATABASE']['host'], 
+                             port=int(config['DATABASE']['port']))
         print("Conexão com o Banco de dados MySQL realizada com sucesso !")
         return db
